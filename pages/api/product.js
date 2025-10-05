a28125f125aaa9c6874ec81a3b7461ad0128f8b6
Ahrar Shah <shahbakers786@gmail.com>
2025-10-05 21:09:39 +0500

    Product Mange

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,4 +1,4 @@
-import { getFile } from '../utils/githubclient';
+import { getFile } from '../utils/github_api_client';
 
 export default async function handler(req, res) {
   const { id } = req.query;
