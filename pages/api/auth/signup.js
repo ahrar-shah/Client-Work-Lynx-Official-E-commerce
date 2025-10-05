fbe54e964997c3976a3efb3e1325587248d06e23
Ahrar Shah <shahbakers786@gmail.com>
2025-10-05 21:10:59 +0500

    Signup Function!

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,5 @@
 // Signup: stores user in GitHub under data/users/<email>.json
-import { createOrUpdateFile, getFile } from '../../utils/githubclient';
+import { createOrUpdateFile, getFile } from '../../utils/github_api_client';
 import { signToken } from '../../../utils/jwt';
 
 export default async function handler(req, res) {
