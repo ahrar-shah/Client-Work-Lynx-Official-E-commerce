012c213fcf579c1b29fe610bb0e6930d2e4861b2
Ahrar Shah <shahbakers786@gmail.com>
2025-10-05 21:10:11 +0500

    Product cards

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,7 +2,7 @@
  * GET: list products
  * POST: add product (admin) - expects base64 image, meta (title, category, price, shortDesc)
  */
-import { getFile, listDir, createOrUpdateFile } from '../utils/githubclient';
+import { getFile, listDir, createOrUpdateFile } from '../utils/github_api_client';
 import { verifyToken } from '../../utils/jwt';
 
 export default async function handler(req, res) {
